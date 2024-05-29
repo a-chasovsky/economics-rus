16938c3854744dcbf654b2cfe0ddef8bc97ecca0
Alexander Chasovsky <alexander.chasovsky@gmail.com>
2024-05-29 16:17:51 +0300

    upd

--- docs/scripts/plotly_plots.js
+++ docs/scripts/plotly_plots.js
@@ -1,4 +1,23 @@
 $(function(){
   $('#price-bread').load('img/price-bread.html'); 
 });
+$(function(){
+  $('#price-beef').load('img/price-beef.html'); 
+});
+$(function(){
+  $('#price-pig').load('img/price-pig.html'); 
+});
+$(function(){
+  $('#price-poultry').load('img/price-poultry.html'); 
+});
+$(function(){
+  $('#price-milk-products').load('img/price-milk-products.html'); 
+});
+$(function(){
+  $('#price-vegetables').load('img/price-vegetables.html'); 
+});
+$(function(){
+  $('#price-eggs').load('img/price-eggs.html'); 
+});
+
 
